5be36000ee30991982108d8a247f29e8eb8f9cdb
scheduled-for-hacker <whtmdgus56@nate.com>
2023-04-07 16:15:25 +0900

    feat: Judge ball, strike, nothing

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -40,7 +40,29 @@ public class Application {
         return arrNum;
     }
 
+    public static int judge(ArrayList<Integer> comNum, ArrayList<Integer> manNum) {
+        int ball = 0;
+        int strike = 0;
+        for (int i = 0; i < manNum.size(); i++) {
+            if (comNum.contains(manNum.get(i))) {
+                if (comNum.get(i) == manNum.get(i)) {
+                    strike += 1;
+                } else {
+                    ball += 1;
+                }
+            }
+        }
 
+        if (ball != 0)
+            System.out.print("" + ball + "볼 ");
+        if (strike != 0)
+            System.out.print("" + strike + "스트라이크 ");
+        if (ball == 0 && strike == 0)
+            System.out.print("낫싱");
+        System.out.println();
+
+        return strike;
+    }
 
     public static void main(String[] args) {
     }
