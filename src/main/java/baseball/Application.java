c0bbe92ec5bd44d6230e510d802adb405db1ca1a
scheduled-for-hacker <whtmdgus56@nate.com>
2023-04-07 15:49:38 +0900

    feat: Man generates random number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -19,6 +19,26 @@ public class Application {
         return numList;
     }
 
+    public static void validCheck(String num) throws IllegalArgumentException{
+
+
+        throw new IllegalArgumentException();
+    }
+
+    public static int manGenNum() throws IllegalArgumentException {
+        Console console = System.console();
+        String num = console.readLine("숫자를 입력해주세요 : ");
+        try {
+            validCheck(num);
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
+
+        return Integer.parseInt(num);
+    }
+
+
+
     public static void main(String[] args) {
     }
 }
