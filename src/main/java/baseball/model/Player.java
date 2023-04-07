1c4d10d372f4d9b4e2c219190b066629457beb50
scheduled-for-hacker <whtmdgus56@nate.com>
2023-04-07 20:11:01 +0900

    refactor: Created (model)Player

---
+++ src/main/java/baseball/model/Player.java
@@ -0,0 +1,20 @@
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private static List<Integer> balls;
+
+    static final int MAX_NUM = 9;
+    static final int MIN_NUM = 1;
+    static final int LENGTH = 3;
+
+    public static void setBalls(ArrayList<Integer> balls) {
+        Player.balls = balls;
+    }
+
+    public static List<Integer> getBalls() {
+        return balls;
+    }
+}
