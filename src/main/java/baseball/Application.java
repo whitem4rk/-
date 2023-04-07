6e22ecee84aecc429f09a06251db3ed6498a10fd
scheduled-for-hacker <whtmdgus56@nate.com>
2023-04-07 15:47:12 +0900

    feat: Computer generates random number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Application {
+
+    public static ArrayList<Integer> comGenNum() {
+        Random random = new Random();
+        ArrayList<Integer> numList = new ArrayList<Integer>();
+        // depth 1
+        while (numList.size() != 3) {
+            int randomNum = random.nextInt(9) + 1;
+            // depth 2
+            if (!numList.contains(randomNum))
+                numList.add(randomNum);
+        }
+        return numList;
+    }
+
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
     }
 }
